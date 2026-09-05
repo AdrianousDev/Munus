6a2174adaf4d897d8d574073301d7ff42ea818cc
bruno j a <brunoadrianousz@gmail.com>
2026-09-05 01:41:08 -0300

    refactor(frontend): remove legacy task implementation

--- react-app/src/contexts/AppProviders.tsx
+++
@@ -1,6 +0,0 @@
-import type { AppProvidersProps } from "../interfaces/props/IAppProvidersProps";
-import { TasksProvider } from "./tasks/TasksProvider";
-
-export const AppProviders = ({ children }: AppProvidersProps) => {
-    return <TasksProvider>{children}</TasksProvider>;
-};
--- react-app/src/contexts/tasks/TasksContext.tsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-import type { TasksContextValue } from "../../interfaces/tasks/ITasksContextValue";
-
-export const TasksContext = createContext<TasksContextValue | null>(null);
